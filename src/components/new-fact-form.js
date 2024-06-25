668b7fabad34ae8e0e7d062fb16122891d28fc86
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-06-25 17:45:18 +0330

    making components

---
+++ src/components/new-fact-form.js
@@ -0,0 +1,75 @@
+import { CATEGORIES } from "../App";
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+export default function NewFactForm({ setFacts, setShowForm }) {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+  const textLength = text.length;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+      setIsUploading(true);
+      const { data: newFact, error } = await supabase
+        .from("facts")
+        .insert([{ text, source, category }])
+        .select();
+      setIsUploading(false);
+
+      if (!error) {
+        setFacts((facts) => [newFact[0], ...facts]);
+      }
+
+      setText("");
+      setSource("");
+      setCategory("");
+      setShowForm(false);
+    }
+  }
+
+  return (
+    <form className='fact-form' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Share a fact with the world...'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        disabled={isUploading}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        value={source}
+        type='text'
+        placeholder='Trustworthy source...'
+        onChange={(e) => setSource(e.target.value)}
+        disabled={isUploading}
+      />
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        disabled={isUploading}
+      >
+        <option value=''>Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className='btn btn-large' disabled={isUploading}>
+        Post
+      </button>
+    </form>
+  );
+}
